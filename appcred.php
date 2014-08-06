3bb6e3c4601be6a053dcfe0360fe2acaa7312253
Kinock Sebastian <kinocksebastian@gmail.com>
2014-08-06 19:02:02 +0530

    Update appcred.php

--- appcred.php
+++ appcred.php
@@ -1,7 +1,7 @@
 <?php
 class appCred{
 	private $clientId = '811775410830-md5k1ke6hautv4agn0uemjr1vd08mve8.apps.googleusercontent.com';
-	private $clientSecret = 'VARYKiH4PhCgxVlSFQrdLZ0m';
+	private $clientSecret = '******************';
 	private $redirectUrl = 'http://organise-inmyway.rhcloud.com';
 	public function getClientId(){
 		return $this->clientId;
